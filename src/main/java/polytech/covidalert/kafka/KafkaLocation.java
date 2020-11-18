5583ae692b2e3d6ef906f22ad80fd23e90b27b68
Alexandre <alexandre.girbal.pro@gmail.com>
2020-11-18 17:58:18 +0100

    using kafka topic to compute some things

--- src/main/java/polytech/covidalert/kafka/KafkaLocation.java
+++ src/main/java/polytech/covidalert/kafka/KafkaLocation.java
@@ -4,12 +4,14 @@ public class KafkaLocation {
     private String userEmail;
     private float longitude;
     private float latitude;
+    private int timestamp;
 
     public KafkaLocation(){}
-    public KafkaLocation(String userEmail, float longitude, float latitude) {
+    public KafkaLocation(String userEmail, float longitude, float latitude, int timestamp) {
         this.userEmail = userEmail;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.timestamp = timestamp;
     }
 
     public String getUserEmail() {
@@ -23,4 +25,6 @@ public class KafkaLocation {
     public float getLatitude() {
         return this.latitude;
     }
+
+    public int getTimestamp() { return this.timestamp; }
 }
--- src/main/java/polytech/covidalert/kafka/KafkaLocationsConsumer.java
+++ src/main/java/polytech/covidalert/kafka/KafkaLocationsConsumer.java
@@ -3,17 +3,55 @@ package polytech.covidalert.kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import polytech.covidalert.models.Location;
+
+import java.util.ArrayList;
 
 @Component
 public class KafkaLocationsConsumer {
     Logger LOG = LoggerFactory.getLogger(KafkaLocationsConsumer.class);
 
+    private int minuteOfTimestamp;
+    private ArrayList<KafkaLocation> setOfLocations;
+    private ArrayList<ArrayList<KafkaLocation>> locationsToBeTreated;
+
+    public KafkaLocationsConsumer() {
+        this.minuteOfTimestamp = 0;
+        this.setOfLocations = new ArrayList<KafkaLocation>();
+        this.locationsToBeTreated = new ArrayList<ArrayList<KafkaLocation>>();
+        for (int i = 0; i < 5; i++) {
+            this.locationsToBeTreated.add(new ArrayList<KafkaLocation>());
+        }
+    }
+
+    public void addSetOfLocations() {
+        try {
+            System.out.println(locationsToBeTreated.toString());
+            for(int i = 5-1; i >= 1; i--){
+                System.out.println(i);
+                locationsToBeTreated.set(i, locationsToBeTreated.get(i-1));
+            }
+            locationsToBeTreated.set(0, setOfLocations);
+            setOfLocations.clear();
+        } catch (Exception e) {
+            System.out.println("Error while adding setOfLocations in locationsToBeTreated:");
+        }
+    }
+
     @KafkaListener(topics = "locations", groupId="15")
-    public void locationsListener(KafkaLocation message) {
-        LOG.info(String.format("##### Locations topic -> Consumed message -> %s / %s / %s", message.getLatitude(), message.getLongitude(), message.getUserEmail()));
-        //on fait un traitement avec ça
-        //puis on renvoie dans un autre topic le resultat du traitement
+    public void locationsListener(@Payload KafkaLocation message) {
+        LOG.info(String.format("##### Locations topic -> Consumed message -> %s / %s / %s / %s", message.getLatitude(), message.getLongitude(), message.getUserEmail(), message.getTimestamp()));
+        System.out.println(String.format("minute: %d",message.getTimestamp()/60));
+        int minute = message.getTimestamp()/60;
+        if (minuteOfTimestamp != minute){ //minute has changed
+            minuteOfTimestamp = minute;
+            addSetOfLocations();
+        }
+        System.out.println(setOfLocations.isEmpty());
+        setOfLocations.add(message);
     }
 }
